4343cda7a5e32ef137655978822cfeeb3e4162c3
Mintu <gmsanzid@gmail.com>
2020-09-22 13:03:26 +0600

    wwww

---
+++ ipcameraoverlay/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ipcameraoverlay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Send = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Send
+            // 
+            this.Send.Location = new System.Drawing.Point(353, 116);
+            this.Send.Name = "Send";
+            this.Send.Size = new System.Drawing.Size(75, 23);
+            this.Send.TabIndex = 0;
+            this.Send.Text = "Send";
+            this.Send.UseVisualStyleBackColor = true;
+            this.Send.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(128, 118);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(219, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Send);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Send;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ ipcameraoverlay/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ipcameraoverlay
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            SendToHikVision("admin", "admin@123", "10.11.1.16", "80");
+        }
+
+        void SendToHikVision(string UserName, string Password, string IP, string Port)
+        {
+            try
+            {
+                HttpWebRequest wr = (HttpWebRequest)WebRequest.Create("http://10.11.1.16//ISAPI/System/Video/inputs/channels/1/overlays/text");
+                wr.Accept = "*/*";
+                wr.Method = "PUT";
+                wr.ReadWriteTimeout = 5000;
+                //wr.UseDefaultCredentials = false;
+                wr.Credentials = new NetworkCredential(UserName, Password);
+
+                byte[] pBytes = GetDHCPPost();
+                wr.ContentLength = pBytes.Length;
+
+                using (Stream DS = wr.GetRequestStream())
+                {
+                    DS.Write(pBytes, 0, pBytes.Length);
+                    DS.Close();
+                }
+                wr.BeginGetResponse(r => { var reponse = wr.EndGetResponse(r); }, null);
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        byte[] GetDHCPPost()
+        {
+            StringBuilder sb = new StringBuilder();
+            
+            sb.AppendLine("<TextOverlayList  version=\"2.0\" xmlns=\"http://www.hikvision.com/ver10/XMLSchema\">");
+            sb.AppendLine("<TextOverlay>");
+            sb.AppendLine("<id>1</id>");
+            sb.AppendLine("<enabled>true</enabled>");
+            sb.AppendLine("<positionX>100</positionX>");
+            sb.AppendLine("<positionY>500</positionY>");
+            sb.AppendLine("<displayText>"+textBox1.Text+" </displayText>");         
+            sb.AppendLine("</TextOverlay>");
+
+          //  sb.AppendLine("<TextOverlay>");
+            //sb.AppendLine("<id>2</id>");
+            //sb.AppendLine("<enabled>true</enabled>");
+            //sb.AppendLine("<positionX>100</positionX>");
+            //sb.AppendLine("<positionY>480</positionY>");
+            //sb.AppendLine("<displayText>product-1 </displayText>");
+            //sb.AppendLine("</TextOverlay>");
+
+
+            //sb.AppendLine("<TextOverlay>");
+            //sb.AppendLine("<id>3</id>");
+            //sb.AppendLine("<enabled>true</enabled>");
+            //sb.AppendLine("<positionX>100</positionX>");
+            //sb.AppendLine("<positionY>440</positionY>");
+            //sb.AppendLine("<displayText>product-2 </displayText>");
+            //sb.AppendLine("</TextOverlay>");
+
+
+            //sb.AppendLine("<TextOverlay>");
+            //sb.AppendLine("<id>4</id>");
+            //sb.AppendLine("<enabled>true</enabled>");
+            //sb.AppendLine("<positionX>100</positionX>");
+            //sb.AppendLine("<positionY>410</positionY>");
+            //sb.AppendLine("<displayText>1500 </displayText>");
+          //  sb.AppendLine("</TextOverlay>");
+
+
+
+
+            sb.AppendLine("</TextOverlayList> ");
+
+
+            return Encoding.ASCII.GetBytes(sb.ToString());
+        }
+    }
+}
